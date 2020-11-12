0c87d8384ee16440a9435df3890cbba5d96e8377
Topicos2020v2 <mikecg.83@gmail.com>
2020-11-12 17:02:57 -0600

    Clase 22

--- src/sample/components/ButtonCustome.java
+++ src/sample/components/ButtonCustome.java
@@ -1,19 +1,42 @@
 package sample.components;
 
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableCell;
 import sample.models.PlatillosDAO;
+
+import java.util.Optional;
 
 public class ButtonCustome extends TableCell<PlatillosDAO,String> {
 
     private Button btnCelda;
+    private PlatillosDAO objPlatillo;
 
     public ButtonCustome(int opc){
         if (opc == 1){
               btnCelda = new Button("Editar");
-            System.out.println("opc: "+opc);
-        }else{btnCelda = new Button("Borrar");
-            System.out.println("opc: "+opc);}
+
+        }else{
+              btnCelda = new Button("Borrar");
+              btnCelda.setOnAction(event -> {
+
+                  Alert alerta = new Alert(Alert.AlertType.CONFIRMATION);
+                  alerta.setTitle("MENSAJE DEL SISTEMA :)");
+                  alerta.setHeaderText("Confirmacion de Acción");
+                  alerta.setContentText("Realmente quieres borrar el registro?");
+                  Optional<ButtonType> result = alerta.showAndWait();
+                  if(result.get() == ButtonType.OK){
+                      //obtenemos el objeto de tipo platillo deacuerdo al reglon selecionado
+                      objPlatillo = ButtonCustome.this.getTableView().getItems().get(ButtonCustome.this.getIndex());
+                      objPlatillo.delPlatillo();
+
+                      //&Actualizamos el tableview
+                      ButtonCustome.this.getTableView().setItems(objPlatillo.getAllPlatillo());
+                      ButtonCustome.this.getTableView().refresh();
+                  }
+              });
+        }
     }
 
     @Override
