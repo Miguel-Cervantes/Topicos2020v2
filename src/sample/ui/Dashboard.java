dd35904553fbf291b7bee7c74944e724f56c92d9
Topicos2020v2 <mikecg.83@gmail.com>
2020-11-13 02:12:54 -0600

    Act y Styles

--- src/sample/ui/Dashboard.java
+++ src/sample/ui/Dashboard.java
@@ -1,21 +1,28 @@
 package sample.ui;
 
+import javafx.scene.Scene;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import sample.models.PlatillosDAO;
 
 public class Dashboard extends Stage {
 
+    private Scene escena;
+    private VBox vBox;
+
     public Dashboard(){
         CrearGUI();
         this.setTitle("Dashboard");
+        this.setScene(escena);
         this.show();
 
         new PlatilloCRUD();
     }
 
     private void CrearGUI() {
-
+        vBox=new VBox();
+        escena=new Scene(vBox,200,100);
     }
 
 }
